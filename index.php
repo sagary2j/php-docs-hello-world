e60d7931b6222e51d0968352c266e6b64499605a
Sagar Ravkhande <sagar.rav89@gmail.com>
2019-05-11 19:00:11 +0200

    updated mysql output

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
+$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "mydrupalserver.mysql.database.azure.com", "myadmin@mydrupalserver", "deployit@123", "sampledb", 3306);
+
 echo "Hello Sagar. You are Rockstar Baby!!!";
